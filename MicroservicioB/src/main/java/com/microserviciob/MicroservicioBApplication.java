e118c290f5dfb869a2aff4455956ca2380112c45
garychavez <52689437+garychavez@users.noreply.github.com>
2019-07-25 15:01:44 -0500

    .

--- MicroservicioB/src/main/java/com/microserviciob/MicroservicioBApplication.java
+++ MicroservicioB/src/main/java/com/microserviciob/MicroservicioBApplication.java
@@ -2,8 +2,11 @@ package com.microserviciob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
+@EnableDiscoveryClient
 public class MicroservicioBApplication {
 
 	public static void main(String[] args) {
